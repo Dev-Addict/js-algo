19dcde9548c3b031aa6c8cbd309d7e77bfeb5607
WolfMAN <aria.azadi.pour@protonmail.com>
2020-10-19 22:20:55 +0330

    selection sort

---
+++ selectionSort.js
@@ -0,0 +1,14 @@
+const selectionSort = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min])
+                min = j;
+        }
+        [arr[min], arr[i]] = [arr[i], arr[min]];
+    }
+
+    return arr;
+};
+
+console.log(selectionSort([34, 22, 10, 17, 13]));
