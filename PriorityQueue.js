f1ef62157b6f25babc9ce3aed27e8c0716bf96fe
WolfMAN <aria.azadi.pour@protonmail.com>
2020-10-31 16:28:26 +0330

    Priority Queue

--- PriorityQueue.js
+++ PriorityQueue.js
@@ -1,16 +1,26 @@
-class MaxBinaryHeap {
+class Node {
+    constructor(val, priority) {
+        this.val = val;
+        this.priority = priority;
+    }
+}
+
+class PriorityQueue {
     constructor() {
         this.values = [];
     }
 
-    insert(val) {
-        this.values.push(val);
+    insert(val, priority) {
+        const node = new Node(val, priority);
+
+        this.values.push(node);
         this.bubbleUp(this.values.length - 1);
     }
 
     bubbleUp(index) {
         const parentIndex = Math.floor((index - 1) / 2);
-        if (this.values[index] > this.values[parentIndex]) {
+
+        if (this.values[index]?.priority < this.values[parentIndex]?.priority) {
             [this.values[index], this.values[parentIndex]] =
                 [this.values[parentIndex], this.values[index]];
             this.bubbleUp(parentIndex);
@@ -32,14 +42,14 @@ class MaxBinaryHeap {
         if (leftChildIndex >= this.values.length) return;
         if (rightChildIndex > this.values.length) return;
         if (rightChildIndex === this.values.length)
-            if (this.values[index] < this.values[leftChildIndex]) {
+            if (this.values[index]?.priority > this.values[leftChildIndex]?.priority) {
                 [this.values[index], this.values[leftChildIndex]] =
                     [this.values[leftChildIndex], this.values[index]];
                 return;
             }
-        if (this.values[leftChildIndex] > this.values[index] ||
-            this.values[rightChildIndex] > this.values[index]) {
-            if (this.values[leftChildIndex] > this.values[rightChildIndex]) {
+        if (this.values[leftChildIndex]?.priority < this.values[index]?.priority ||
+            this.values[rightChildIndex]?.priority < this.values[index]?.priority) {
+            if (this.values[leftChildIndex] < this.values[rightChildIndex]) {
                 [this.values[leftChildIndex], this.values[index]] =
                     [this.values[index], this.values[leftChildIndex]];
                 this.bubbleUp(leftChildIndex);
@@ -51,3 +61,14 @@ class MaxBinaryHeap {
         }
     }
 }
+
+const priorityQueue = new PriorityQueue();
+
+priorityQueue.insert('task 1', 2);
+priorityQueue.insert('task 2', 1);
+priorityQueue.insert('task 3', 3);
+priorityQueue.insert('task 4', 3);
+
+priorityQueue.remove(1);
+
+console.log(priorityQueue.values);
